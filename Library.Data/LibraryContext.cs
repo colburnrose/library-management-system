699e88539d6e0aeb66cbbc39e28661cfb4b9a81f
Colburn Sanders <colburnsanders@gmail.com>
2018-11-15 12:59:26 -0600

    'setup'

---
+++ Library.Data/LibraryContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Library.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Data
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        //map to the entity objs properties and those columns listed in the db tables.
+        public DbSet<Patron> Patrons { get; set; }
+    }
+}
